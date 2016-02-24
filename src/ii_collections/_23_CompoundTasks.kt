2a55d213fceac28900ca456d2eab97093e235f4a
Nils Breunese <nils@breun.nl>
2016-02-24 20:30:08 +0100

    ii collections - 23 CompoundTasks (operator overloading)

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -2,7 +2,7 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    return customers.filter { it.orderedProducts.contains(product) }.toSet()
+    return customers.filter { product in it.orderedProducts }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
